f0cac52e97d2bc09241bd7b9e6f5c9921cf279aa
Quentin Gliech <quentingliech@gmail.com>
2018-11-03 16:29:26 +0100

    Handle Linux SLL devices (-i any)

--- link.c
+++ link.c
@@ -42,6 +42,28 @@ static void handle_raw(const uint32_t length, const uint8_t *packet) {
   }
 }
 
+// Linux SLL devices, i.e. when capturing on the `any` interface
+#ifdef DLT_LINUX_SLL
+struct linux_sll_header {
+  uint16_t sent_by;
+  uint16_t address_type;
+  uint16_t address_length;
+  uint8_t source_address[8];
+  uint16_t ether_type;
+};
+
+static void handle_linux_sll(uint32_t length, const uint8_t *packet) {
+  // TODO: check the address type
+  struct linux_sll_header *linux_sll = (struct linux_sll_header *)packet;
+  APPLY_OVERHEAD(struct linux_sll_header, length, packet);
+  DEBUGF("Linux SLL packet addr: %s, type: 0x%04x, length: %d",
+         ether_ntoa((struct ether_addr *)linux_sll->source_address),
+         htons(linux_sll->ether_type),
+         length);
+  handle_ether_payload(htons(linux_sll->ether_type), length, packet);
+}
+#endif
+
 // Loopback devices
 #ifdef DLT_NULL
 uint16_t af_to_ethertype(u_int16_t af) {
@@ -73,7 +95,7 @@ static void handle_null(uint32_t length, const uint8_t *packet) {
   struct null_header* null = (struct null_header *)packet;
   APPLY_OVERHEAD(struct null_header, length, packet);
 
-  DEBUGF("Null packet type: %#08x, length: %d", null->af_type, length);
+  DEBUGF("Null packet type: 0x%04x, length: %d", null->af_type, length);
   handle_ether_payload(af_to_ethertype(null->af_type), length, packet);
 }
 #endif
@@ -84,7 +106,7 @@ static void handle_ethernet(uint32_t length, const uint8_t *packet) {
   struct ether_header *ethernet = (struct ether_header *)packet;
   APPLY_OVERHEAD(struct ether_header, length, packet);
 
-  DEBUGF("Ethernet packet dst: %s, src: %s, type: %#08x, length: %d",
+  DEBUGF("Ethernet packet dst: %s, src: %s, type: 0x%04x, length: %d",
          ether_ntoa((struct ether_addr *)ethernet->ether_dhost),
          ether_ntoa((struct ether_addr *)ethernet->ether_shost),
          htons(ethernet->ether_type),
@@ -102,6 +124,9 @@ static link_handler handlers[] = {
 #endif
 #ifdef DLT_RAW
   [DLT_RAW] = handle_raw,
+#endif
+#ifdef DLT_LINUX_SLL
+  [DLT_LINUX_SLL] = handle_linux_sll,
 #endif
 };
 
